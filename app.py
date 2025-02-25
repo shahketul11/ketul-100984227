a3edb6d580b0141642a8768632827684664543d2
shahketul11 <ketulshah390@gmail.com>
2025-02-25 16:28:42 -0500

    latest

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Cloud Run!"
+    return "Hello Cloud Run! Name: Ketul Shah and Student ID:100984227"
 
 if __name__ == "__main__": 
     app.run(host="0.0.0.0", port=8080)
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.send("Hello Cloud Run!");
+  res.send("Hello Cloud Run! Name: Ketul Shah and Student ID:100984227");
 });
 
 app.listen(PORT, () => {
